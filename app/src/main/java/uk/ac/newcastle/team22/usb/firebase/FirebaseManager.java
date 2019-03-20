7882c3fd69b8d6b99a62baf730b98aa1fdc95aef
Alexander MacLeod <alexandermacleod@me.com>
2019-03-20 12:53:51 +0000

    Temporary user login fix

--- app/src/main/java/uk/ac/newcastle/team22/usb/firebase/FirebaseManager.java
+++ app/src/main/java/uk/ac/newcastle/team22/usb/firebase/FirebaseManager.java
@@ -8,6 +8,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GetTokenResult;
 import com.google.firebase.firestore.*;
 
 import java.util.ArrayList;
@@ -138,18 +139,23 @@ public class FirebaseManager {
      */
     public void authenticate(final FirestoreCompletionHandler<Void> handler) {
         FirebaseUser currentUser = authentication.getCurrentUser();
+        /*
         if (currentUser != null) {
             handler.completed(null);
             return;
         }
+        */
 
         // Sign into Firebase anonymously.
         authentication.signInAnonymously().addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
+                    Log.i("", "Successful");
                     handler.completed(null);
                 } else {
+                    Log.i("", "Failed");
+                    Log.i("", task.getException().getLocalizedMessage());
                     handler.failed(task.getException());
                 }
             }
