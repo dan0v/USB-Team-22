4157c32152f432a729077a837c81230056e568a0
Daniel Walker <danielmarkwalker18@hotmail.com>
2019-04-13 16:50:47 +0100

    Updated comments for consistency

--- app/src/test/java/uk/ac/newcastle/team22/usb/AResourceShould.java
+++ app/src/test/java/uk/ac/newcastle/team22/usb/AResourceShould.java
@@ -15,18 +15,21 @@ import static org.junit.Assert.*;
  */
 public class AResourceShould {
 
+    /** Assert a resource is of one of the types defined in the enum 'ResourceType' */
     @Test
     public void haveAType() {
         Resource computer = new Resource(ResourceType.COMPUTER, 5);
         assertEquals(ResourceType.COMPUTER, computer.getType());
     }
 
+    /** Assert a resource can display the total number available */
     @Test
     public void haveAStatus() {
         Resource computer = new Resource(ResourceType.COMPUTER, 5);
         assertEquals(5, computer.getAvailable());
     }
 
+    /** Assert a resource can display the total number present */
     @Test
     public void haveATotal() {
         Resource computer = new Resource(ResourceType.COMPUTER, 5, 5);
