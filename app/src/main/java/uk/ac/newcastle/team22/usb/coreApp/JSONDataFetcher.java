a2e0c5e0d74622c82efc56c56c9d687f3c89e81e
dan0v <7658521+dan0v@users.noreply.github.com>
2019-04-23 16:08:49 +0200

    Added update interval to JSONDataFetcher.

--- app/src/main/java/uk/ac/newcastle/team22/usb/coreApp/JSONDataFetcher.java
+++ app/src/main/java/uk/ac/newcastle/team22/usb/coreApp/JSONDataFetcher.java
@@ -17,6 +17,7 @@ import com.google.gson.JsonParser;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.Calendar;
 import java.util.Map;
 
 import javax.net.ssl.HostnameVerifier;
@@ -52,6 +53,9 @@ public class JSONDataFetcher extends AsyncTask<Void, Void, Void> {
     /** The response reference. */
     private AsyncResponse reference;
 
+    /** The interval at which to update computer availability in milliseconds - 5 minutes. */
+    private final int UPDATE_INTERVAL = 300000;
+
     /**
      * @param activity The current activity.
      * @param reference UI activity fetching JSON update.
@@ -64,57 +68,62 @@ public class JSONDataFetcher extends AsyncTask<Void, Void, Void> {
     @Override
     public Void doInBackground(Void... voids) {
         try {
-            // Initial check for the correct WIFI SSID.
-            WifiInfo wifiInfo;
-            wifiInfo = wifiManager.getConnectionInfo();
-            if (wifiInfo.getSupplicantState() == SupplicantState.COMPLETED) {
-                if (!wifiInfo.getSSID().equals(NEWCASTLE_WIFI_SSID) && !wifiInfo.getSSID().equals(UNKNOWN_WIFI_SSID)) {
-                    throw new java.net.UnknownHostException();
-                }
-            }
-
-            // Request computer availability.
-            URL url = new URL(NUIT_ADDRESS);
-            HttpsURLConnection connection = (HttpsURLConnection ) url.openConnection();
-            connection.setSSLSocketFactory(SSLCertificateSocketFactory.getInsecure(0, null));
-            connection.setHostnameVerifier(getHostnameVerifier());
-            connection.connect();
-
-            JsonParser parser = new JsonParser();
-            JsonObject base = parser.parse(new InputStreamReader((InputStream) connection.getContent())).getAsJsonObject();
-            JsonArray locationSummary = base.get("usb_location_summary").getAsJsonArray();
-            Map<Integer, Floor> buildingFloors = USBManager.shared.getBuilding().getFloors();
-
-            for (JsonElement location : locationSummary) {
-                JsonObject locationObject = location.getAsJsonObject();
-
-                int floor = Integer.parseInt(locationObject.get("location_name").getAsString().substring(4,5));
-                String number = locationObject.get("location_name").getAsString().substring(6,9);
-                String formattedNumber = number;
-                int availableComputers = Integer.parseInt(locationObject.get("location_free").getAsString());
-                int totalComputers = Integer.parseInt(locationObject.get("location_total").getAsString());
-
-                // Remove leading 0's from room names.
-                for (int i = 0; i < number.length() - 1; i++) {
-                    if (formattedNumber.charAt(0) == '0') {
-                        formattedNumber = formattedNumber.substring(1);
-                    }
-                    else {
-                        break;
+            // Do not update if last update was less than UPDATE_INTERVAL milliseconds ago.
+            if (USBManager.shared.getBuilding().getLastJSONUpdateTime() == null || Calendar.getInstance().getTimeInMillis() - USBManager.shared.getBuilding().getLastJSONUpdateTime().getTimeInMillis() > UPDATE_INTERVAL) {
+                // Initial check for the correct WIFI SSID.
+                WifiInfo wifiInfo;
+                wifiInfo = wifiManager.getConnectionInfo();
+                if (wifiInfo.getSupplicantState() == SupplicantState.COMPLETED) {
+                    if (!wifiInfo.getSSID().equals(NEWCASTLE_WIFI_SSID) && !wifiInfo.getSSID().equals(UNKNOWN_WIFI_SSID)) {
+                        throw new java.net.UnknownHostException();
                     }
                 }
 
-                Resource temp = new Resource(ResourceType.COMPUTER, availableComputers, totalComputers);
-                try {
-                    buildingFloors.get(floor).getRooms().get(formattedNumber).updateComputerAvailability(temp);
-                    Log.d("JSON Updater", "Floor: " + floor + " Room: " + formattedNumber + "'s available computer data has been updated");
-                } catch (Exception e) {
-                    Log.e("JSON Updater", "Exception: " + e + ", Room might be missing from Firestore: Floor: " + floor + " Room: " + number + ", " + formattedNumber);
-                }
-            }
+                // Request computer availability.
+                URL url = new URL(NUIT_ADDRESS);
+                HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+                connection.setSSLSocketFactory(SSLCertificateSocketFactory.getInsecure(0, null));
+                connection.setHostnameVerifier(getHostnameVerifier());
+                connection.connect();
 
-            // Successful update.
-            reference.onComplete();
+                JsonParser parser = new JsonParser();
+                JsonObject base = parser.parse(new InputStreamReader((InputStream) connection.getContent())).getAsJsonObject();
+                JsonArray locationSummary = base.get("usb_location_summary").getAsJsonArray();
+                Map<Integer, Floor> buildingFloors = USBManager.shared.getBuilding().getFloors();
+
+                for (JsonElement location : locationSummary) {
+                    JsonObject locationObject = location.getAsJsonObject();
+
+                    int floor = Integer.parseInt(locationObject.get("location_name").getAsString().substring(4, 5));
+                    String number = locationObject.get("location_name").getAsString().substring(6, 9);
+                    String formattedNumber = number;
+                    int availableComputers = Integer.parseInt(locationObject.get("location_free").getAsString());
+                    int totalComputers = Integer.parseInt(locationObject.get("location_total").getAsString());
+
+                    // Remove leading 0's from room names.
+                    for (int i = 0; i < number.length() - 1; i++) {
+                        if (formattedNumber.charAt(0) == '0') {
+                            formattedNumber = formattedNumber.substring(1);
+                        } else {
+                            break;
+                        }
+                    }
+
+                    Resource temp = new Resource(ResourceType.COMPUTER, availableComputers, totalComputers);
+                    try {
+                        buildingFloors.get(floor).getRooms().get(formattedNumber).updateComputerAvailability(temp);
+                        Log.d("JSON Updater", "Floor: " + floor + " Room: " + formattedNumber + "'s available computer data has been updated");
+                    } catch (Exception e) {
+                        Log.e("JSON Updater", "Exception: " + e + ", Room might be missing from Firestore: Floor: " + floor + " Room: " + number + ", " + formattedNumber);
+                    }
+                }
+
+                // Set last known update time to current time.
+                USBManager.shared.getBuilding().setLastJSONUpdateTime(Calendar.getInstance());
+
+                // Successful update.
+                reference.onComplete();
+            }
 
         } catch (Exception e) {
             // Notify UI that there is a network issue.
