4374102e0cb6ae00ca41ff1146ddd43b2733d3f2
Alexander MacLeod <alexandermacleod@me.com>
2019-04-24 18:04:16 +0100

    Fixed compass code alignment

--- app/src/main/java/uk/ac/newcastle/team22/usb/navigation/Compass.java
+++ app/src/main/java/uk/ac/newcastle/team22/usb/navigation/Compass.java
@@ -35,7 +35,7 @@ public class Compass implements SensorEventListener {
 
     private float[] R = new float[9];
     private float[] I = new float[9];
-
+    
     private float azimuth;
     private int azimuthOffset;
 
@@ -75,20 +75,14 @@ public class Compass implements SensorEventListener {
         final float alpha = 0.97f;
         synchronized (this) {
             if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-                mGravity[0] = alpha * mGravity[0] + (1 - alpha)
-                        * event.values[0];
-                mGravity[1] = alpha * mGravity[1] + (1 - alpha)
-                        * event.values[1];
-                mGravity[2] = alpha * mGravity[2] + (1 - alpha)
-                        * event.values[2];
+                mGravity[0] = alpha * mGravity[0] + (1 - alpha) * event.values[0];
+                mGravity[1] = alpha * mGravity[1] + (1 - alpha) * event.values[1];
+                mGravity[2] = alpha * mGravity[2] + (1 - alpha) * event.values[2];
             }
             if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
-                mGeomagnetic[0] = alpha * mGeomagnetic[0] + (1 - alpha)
-                        * event.values[0];
-                mGeomagnetic[1] = alpha * mGeomagnetic[1] + (1 - alpha)
-                        * event.values[1];
-                mGeomagnetic[2] = alpha * mGeomagnetic[2] + (1 - alpha)
-                        * event.values[2];
+                mGeomagnetic[0] = alpha * mGeomagnetic[0] + (1 - alpha) * event.values[0];
+                mGeomagnetic[1] = alpha * mGeomagnetic[1] + (1 - alpha) * event.values[1];
+                mGeomagnetic[2] = alpha * mGeomagnetic[2] + (1 - alpha)  * event.values[2];
             }
             boolean success = SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic);
             if (success) {
